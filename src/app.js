788bcd979dfc5309fb5bd37637fffb5397e3df66
Me <technologiechmurowe2018@o2.pl>
2018-11-04 03:14:26 +0100

    last commit

--- src/app.js
+++ src/app.js
@@ -1,17 +1,34 @@
 var express = require('express')
-var app = express()
- 
+var app = express();
+var Joi = require('joi');
 var fs = require('fs');
 var http = require('http');
 var https = require('https');
 var privateKey  = fs.readFileSync(__dirname +'/cert/server.key', 'utf8');
 var certificate = fs.readFileSync(__dirname +'/cert/server.crt', 'utf8');
+const bcrypt = require('bcrypt');
+
 
 var credentials = {key: privateKey, cert: certificate};
 
+var dynamo = require('dynamodb');
+dynamo.AWS.config.update({accessKeyId: 'AKIAIFMRXFSV63UFW5SA', secretAccessKey: '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', region: "us-west-2"});
+
+Sessions = [];
+
+function validToken(token){
+    Sess = Sessions.filter(sess =>{
+        return sess.token==token;
+    });
+    return Sess.length != 1 ? undefined : Sess[0];
+}
+
 app.get('/', function (req, res) {
-    console.log(req);
     res.sendFile(__dirname +'/html/index.html')
+})
+
+app.get('/crosshair.png', function (req, res) {
+    res.sendFile(__dirname +'/images/crosshair.png')
 })
 
 app.get('/index.css', function(_, res) {
@@ -22,8 +39,6 @@ app.get('/main.js', function(_, res) {
     res.sendFile(__dirname + '/main.js');
 });
 
-
-
 app.get('/login', function(_, res) {
     res.sendFile(__dirname + '/html/login.html');
 });
@@ -32,10 +47,169 @@ app.get('/login.css', function(_, res) {
     res.sendFile(__dirname + '/css/login.css');
 });
 
-app.post('/login/credentials', function(req, res) {
-    console.log(req);
+
+var Accounts = dynamo.define('Account', {
+    hashKey : 'Username',
+    timestamps : true,
+    schema : { 
+        Username    : Joi.string(),
+        Password    : Joi.string(),
+    }
+  });
+
+var Places = dynamo.define('Places', {
+    hashKey : 'Name',
+    timestamps : true,
+    schema : { 
+        Name : Joi.string(),
+        Username : Joi.string(),
+        Lat : Joi.number(),
+        Lng : Joi.number(),
+        ShopList : Joi.string(),
+    }
+});
+
+Places.config({tableName: 'Places'});
+Accounts.config({tableName: 'Accounts'});
+
+app.post('/endsession', function(req, res) {
+    var Sess = validToken(req.query['token']);
+    if(Sess)
+        Sessions.splice(Sessions.indexOf(Sess),1);
     res.send('');
 });
+
+app.get('/getplaces', function(req, res) {
+    var Sess = validToken(req.query['token']);
+    if(Sess){
+        Places.scan().loadAll().exec(
+            function(err,result){
+                if(err){
+                    res.status=401;
+                    res.json({});
+                }
+                else{
+                    res.json(result.Items.map(el=>{
+                        if(el.attrs.Username==Sess.Username)
+                            return {
+                                Name: el.attrs.Name,
+                                LatLng: [el.attrs.Lat,el.attrs.Lng],
+                                ShopList: el.attrs.ShopList,
+                            };
+                    }).filter(el=>{return el!=null}));
+                }
+            }
+        );
+    }
+    else{
+        res.status(401);
+        res.send('');
+    }
+});
+app.post('/newplace', function(req, res) {
+    var Sess = validToken(req.query['token']);
+    if(Sess){
+        Places.create({       
+            Name : req.query['Name'],
+            Username : Sess.Username,
+            Lat : req.query['Lat'],
+            Lng : req.query['Lng'],
+            ShopList :'Empty',
+        },{overwrite : false},function(err,plc){
+            if(err)
+                res.status(409);
+            res.send('');
+        });
+        
+    }
+    else{
+        res.status(401);
+        res.send('');
+    }
+});
+app.post('/updateplace', function(req, res) {
+    var Sess = validToken(req.query['token']);
+    if(Sess){
+        Places.update({        
+            Name : req.query['Name'],
+            Username : Sess.Username,
+            Lat : req.query['Lat'],
+            Lng : req.query['Lng'],
+            ShopList :req.query['ShopList'],
+        });
+        res.send('');
+    }
+    else{
+        res.status(401);
+        res.send('');
+    }
+});
+app.post('/removeplace', function(req, res) {
+    var Sess = validToken(req.query['token']);
+    if(Sess){
+        Places.destroy(req.query['Name'],function (err) {
+            console.log('Place deleted');
+        });
+        res.send('');
+    }
+    else{
+        res.status(401);
+        res.send('');
+    }
+});
+
+app.post('/login/credentials', function(req, res) {
+
+    Accounts.get(req.query['usr'],function(err,model){
+        if(err){
+            console.log(err);
+            res.status(401);
+            res.send('');
+        }
+        else{
+            if((req.query['new'] && model) || (!req.query['new'] && !model)){
+                res.status(401);
+                res.send('');
+                return;
+            }
+            if(req.query['new']){
+                bcrypt.hash(req.query['pass'],10,function(err,hash){
+                    Accounts.create({       
+                        Username : req.query['usr'],
+                        Password : hash,
+                    },{overwrite : false},function(err,plc){
+                        if(err)
+                            res.status(409);
+                        res.send('');
+                    });
+                })
+            }
+            else
+            bcrypt.compare(req.query['pass'], model.attrs['Password'], function(err, result) {
+                if(err || !result){
+                    console.log(err);
+                    res.status(401);
+                    res.send('');
+                }
+                else{
+                    bcrypt.hash(new Date().getTime() + model.attrs['Password'],10,function(err,encrypted){
+                        if(err){
+                            console.log(err);
+                            res.status(401);
+                            res.send('');
+                            return;
+                        }
+                        Sessions.push({Username:model.attrs['Username'],token:encrypted});
+                        res.send(encrypted);
+                    });
+                }
+            });
+        }
+    });
+    
+});
+
+
 
 var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
--- src/main.js
+++ src/main.js
@@ -1,8 +1,35 @@
+
+function LogOut(event){
+    $.ajax({
+        async: false,
+        type: 'POST',
+        url: "/endsession?token="+sessionStorage['token'],
+        success: function(err,res,req){
+            sessionStorage.removeItem('token');
+            window.location.reload();
+        }
+    });
+}
+
+var markers = [];
+$.ajax({
+    async: false,
+    type: 'GET',
+    url: '/getplaces?token='+sessionStorage['token'],
+    success: function(err,res,req) {
+         markers = req.responseJSON;
+    },
+    error: function(err,res,req) {
+        console.log(err);
+        LogOut();
+    }
+});
+
 var map = L.map('map', {
     center: [53.4285438,14.5528116],
     zoom: 13
 });
-
+map.doubleClickZoom.disable();
 var x = document.getElementById("loc");
 
 function showPosition(position) {
@@ -12,7 +39,10 @@ function showPosition(position) {
 }
 
 if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
+    navigator.geolocation.getCurrentPosition(showPosition,function(){
+        l = map.getCenter();
+        x.innerHTML = "Latitude: " +l.lat + " Longitude: " + l.lng;  
+    });
 } else {
     x.innerHTML = "Geolocation is not supported by this browser.";
 }
@@ -24,14 +54,109 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
     accessToken: 'your.mapbox.access.token'
 }).addTo(map);
 
+// Add in a crosshair for the map
+var crosshairIcon = L.icon({
+    iconUrl: 'crosshair.png',
+    iconSize:     [20, 20], // size of the icon
+    iconAnchor:   [10, 10], // point of the icon which will correspond to marker's location
+});
+crosshair = new L.marker(map.getCenter(), {icon: crosshairIcon, clickable:false});
+crosshair.addTo(map);
+
 map.on('drag',function(){
     l = this.getCenter();
+    crosshair.setLatLng(l);
     x.innerHTML = "Latitude: " +l.lat + " Longitude: " + l.lng;  
 });
 
 
+function newMarker(markerData){
+    var marker = L.marker(markerData.LatLng,{draggable:true});
+    popUpContent = document.createElement('div');
+    popUpContent.innerHTML = "<b>"+markerData.Name+"</b><br>"
+    textArea = document.createElement('textarea');
+    textArea.rows = 10;
+    textArea.value = markerData.ShopList;
+    popUpContent.appendChild(textArea);
+    marker.bindPopup(popUpContent);
+    marker.getPopup().originalValue = markerData;
+    marker.getPopup().textAreaRef = textArea;
+    marker.on('dragend',function(event){
+        popUp = this.getPopup();
+        $.ajax({
+            type: "POST",
+            url: '/updateplace?'+'token='+sessionStorage['token']+'&Name='+popUp.originalValue.Name+'&Lat='+event.target.getLatLng().lat+'&Lng='+event.target.getLatLng().lng+'&ShopList='+popUp.textAreaRef.value,
+            success: function(err,res,req) {
+            },
+            error: function(err,res,req) {
+                console.log(err);
+                LogOut();
+            },
+        });
+    });
+    marker.on('contextmenu',function(event){
+        event.originalEvent.preventDefault();
+        popUp = this.getPopup();
 
-/*
-get
+        $.ajax({
+            type: "POST",
+            url: '/removeplace?'+'token='+sessionStorage['token']+'&Name='+popUp.originalValue.Name,
+            success: function(err,res,req) {
+            },
+            error: function(err,res,req) {
+                console.log(err);
+                LogOut();
+            },
+        });
+        this.remove();
+    });
+    
+    marker.on('popupclose',function(event){
+        popUp = this.getPopup();
+        if(popUp.originalValue.ShopList != popUp.textAreaRef.value){
+            if(popUp.textAreaRef.value.length==0)
+                popUp.textAreaRef.value = 'Empty';
+            $.ajax({
+                type: "POST",
+                url: '/updateplace?'+'token='+sessionStorage['token']+'&Name='+popUp.originalValue.Name+'&Lat='+event.target.getLatLng().lat+'&Lng='+event.target.getLatLng().lng+'&ShopList='+popUp.textAreaRef.value,
+                success: function(err,res,req) {
+                },
+                error: function(err,res,req) {
+                    console.log(err);
+                    LogOut();
+                },
+            });
+        }
+    });
 
-'https://www.google.com/maps/search/?api=1&parameters'*/
+    marker.addTo(map);
+}
+
+for(let i=0;i<markers.length;i++){
+    newMarker(markers[i]);
+}
+
+map.on('dblclick',function(event){
+    var name = prompt("Please enter name of marker");
+    if(name==null)
+        return false;
+    $.ajax({
+        type: "POST",
+        url: '/newplace?'+'token='+sessionStorage['token']+'&Name='+name+'&Lat='+event.latlng.lat+'&Lng='+event.latlng.lng,
+        success: function(err,res,req) {
+            newMarker({
+                Name:name,
+                ShopList:'Empty',
+                LatLng: [event.latlng.lat,event.latlng.lng],
+            });
+        },
+        error: function(err,res,req) {
+            if(err.status==401)
+                LogOut();
+            else{
+                alert('Name Taken');    
+            }
+        },
+    });
+    return false;
+});
